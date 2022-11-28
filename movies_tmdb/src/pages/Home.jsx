cabc31c31c60a0f7485702359ef49d4080f9124f
VictoriaCardos <vvjcerque@hotmail.com>
2022-11-27 22:31:45 -0300

    feat: completed api request

--- movies_tmdb/src/pages/Home.jsx
+++ movies_tmdb/src/pages/Home.jsx
@@ -1,7 +1,33 @@
-import React from 'react'
+import { useState, useEffect } from 'react'
+
+const moviesURL = import.meta.env.VITE_API
+const apiKey = import.meta.env.VITE_API_KEY
 
 const Home = () => {
-  return <div>Home</div>
+  const [topMovies, setTopMovies] = useState([])
+
+  const getTopRateMovies = async url => {
+    const response = await fetch(url)
+    const data = await response.json()
+    setTopMovies(data.results)
+    console.log(data.results)
+  }
+
+  useEffect(() => {
+    const topRateUrl = `${moviesURL}top_rated?api_key=${apiKey}`
+
+    getTopRateMovies(topRateUrl)
+  }, [])
+
+  return (
+    <div className="container">
+      <h2 className="title">Melhores Filmes</h2>
+      <div className="movies-container">
+        {topMovies.length > 0 &&
+          topMovies.map(movie => <p key={movie.id}>{movie.title}</p>)}
+      </div>
+    </div>
+  )
 }
 
 export default Home
