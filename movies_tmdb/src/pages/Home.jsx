38d20505721bbfb00634ff52a7e10fd245cb873f
VictoriaCardos <vvjcerque@hotmail.com>
2022-11-27 23:07:15 -0300

    refactor: add movie card component

--- movies_tmdb/src/pages/Home.jsx
+++ movies_tmdb/src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react'
+import MovieCard from '../components/MovieCard'
 
 const moviesURL = import.meta.env.VITE_API
 const apiKey = import.meta.env.VITE_API_KEY
@@ -24,7 +25,7 @@ const Home = () => {
       <h2 className="title">Melhores Filmes</h2>
       <div className="movies-container">
         {topMovies.length > 0 &&
-          topMovies.map(movie => <p key={movie.id}>{movie.title}</p>)}
+          topMovies.map(movie => <MovieCard key={movie.id} movie={movie} />)}
       </div>
     </div>
   )
