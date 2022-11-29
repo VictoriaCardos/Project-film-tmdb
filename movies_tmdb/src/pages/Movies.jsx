ef2ba6f1fd0fbb566db48f05e7bf67aa86b9a884
VictoriaCardos <vvjcerque@hotmail.com>
2022-11-28 22:55:28 -0300

    feat: create page for chosen movie

--- movies_tmdb/src/pages/Movies.jsx
+++ movies_tmdb/src/pages/Movies.jsx
@@ -1,7 +1,76 @@
-import React from 'react'
+import { useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+import {
+  BsGraphUp,
+  BsWallet,
+  BsHourglassSplit,
+  BsFillFileEarmarkTextFill
+} from 'react-icons/bs'
+
+import MovieCard from '../components/MovieCard'
+
+import './Movies.css'
+
+const moviesURL = import.meta.env.VITE_API
+const apiKey = import.meta.env.VITE_API_KEY
 
 const Movies = () => {
-  return <div>Movies</div>
+  const { id } = useParams()
+  const [movie, setMovie] = useState(null)
+
+  const getMovie = async url => {
+    const res = await fetch(url)
+    const data = await res.json()
+    console.log(data)
+    setMovie(data)
+  }
+
+  const formatCurrency = number => {
+    return number.toLocaleString('en-US', {
+      style: 'currency',
+      currency: 'USD'
+    })
+  }
+
+  useEffect(() => {
+    const movieUrl = `${moviesURL}${id}?api_key=${apiKey}`
+    getMovie(movieUrl)
+  }, [])
+
+  return (
+    <div className="movie-page">
+      {movie && (
+        <>
+          <MovieCard movie={movie} showLink={false} />
+          <p className="tagline">{movie.tagline}</p>
+          <div className="info">
+            <h3>
+              <BsWallet /> Orçamento:
+            </h3>
+            <p>{movie.budget}</p>
+          </div>
+          <div className="info">
+            <h3>
+              <BsGraphUp /> Receita:
+            </h3>
+            <p>{movie.revenue}</p>
+          </div>
+          <div className="info">
+            <h3>
+              <BsHourglassSplit /> Duração:
+            </h3>
+            <p>{movie.runtime} minutos</p>
+          </div>
+          <div className="info description">
+            <h3>
+              <BsFillFileEarmarkTextFill /> Descrição:
+            </h3>
+            <p>{movie.overview} minutos</p>
+          </div>
+        </>
+      )}
+    </div>
+  )
 }
 
 export default Movies
